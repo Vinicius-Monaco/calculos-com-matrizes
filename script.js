4a96e3fe526c52ed06a42452cbdde9512f7414a1
Vinicius-Monaco <94020083+Vinicius-Monaco@users.noreply.github.com>
2022-09-27 01:32:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("soma").addEventListener("click", function aparecer() {
+
     document.querySelector(".soma").classList.remove("hidden");
     document.querySelector(".container").classList.add("hidden");
 });
@@ -11,10 +12,10 @@ document.getElementById("btn-define").addEventListener("click", function definir
     var linhaMatriz2 = document.getElementById("linmatriz2").value;
     var input = document.createElement("input");
     var quebraLinha = document.createElement("br");
-    if(colunaMatriz1 != colunaMatriz2 || linhaMatriz1 != linhaMatriz2) {
-        alert("AS MATRIZES PRECISAM SER DE MESMO TAMANHO");
-        this.close();  // Nem sei se isso existe, mas o código parou de executar, então deixei :)
-        
+
+    if((colunaMatriz1 != colunaMatriz2 || linhaMatriz1 != linhaMatriz2) || (colunaMatriz1 <= 0 || colunaMatriz2 <=0 || linhaMatriz1 <= 0 || linhaMatriz2 <= 0)) {
+        alert("MATRIZES DIFERENTES OU ALGUM CAMPO COM NÚMERO NULO OU NEGATIVO!!");
+        this.close();  // Nem sei se isso existe, mas o código parou de executar, então deixei :D
     }
 
     for (var aux1 = 1 ; aux1 <= linhaMatriz1 ; aux1++)
@@ -22,7 +23,7 @@ document.getElementById("btn-define").addEventListener("click", function definir
         for (var aux2 = 1 ; aux2 <= colunaMatriz1 ; aux2++)
         {
             var input = document.createElement("input");
-            input.setAttribute("id", "1c-"+aux2+"l-"+aux1); //  MANO EU SOU UM GENIO PUTAMERDA
+            input.setAttribute("id", "1c-"+aux2+"l-"+aux1); 
             document.getElementById("a").appendChild(input);
         }
         var quebraLinha = document.createElement("br");
@@ -40,10 +41,10 @@ document.getElementById("btn-define").addEventListener("click", function definir
         var quebraLinha = document.createElement("br");
         document.getElementById("b").appendChild(quebraLinha);
     }
-
 });
 
 document.getElementById("btn-soma").addEventListener("click", function somar() {
+
     document.querySelector(".resultado").classList.remove("hidden");
     document.querySelector(".container").classList.add("hidden");
     document.querySelector(".soma").classList.add("hidden");
@@ -51,27 +52,17 @@ document.getElementById("btn-soma").addEventListener("click", function somar() {
     var colunaMatriz3 = document.getElementById("colmatriz1").value;
     var linhaMatriz3 = document.getElementById("linmatriz1").value;
 
-    // for (var cont1 = 1 ; cont1 <= colunaMatriz3 ; cont1++)
-    // {
-    //     for (var cont2 = 1 ; cont2 <= linhaMatriz3 ; cont2++)
-    //     {
-            
-    //     }
-    // }
-
     for (var aux1 = 1 ; aux1 <= linhaMatriz3 ; aux1++)
     {
         for (var aux2 = 1 ; aux2 <= colunaMatriz3 ; aux2++)
         {
             var span = document.createElement("span");
             span.textContent = parseInt(document.getElementById("1c-"+aux2+"l-"+aux1).value) + parseInt(document.getElementById("2c-"+aux2+"l-"+aux1).value);
-
-            span.setAttribute("id", "3c-"+aux2+"l-"+aux1); //  MANO EU SOU UM GENIO PUTAMERDA
+            span.setAttribute("id", "3c-"+aux2+"l-"+aux1);
             document.getElementById("c").appendChild(span);
         }
         var quebraLinha = document.createElement("br");
         document.getElementById("c").appendChild(quebraLinha);
     }
-
-})
+});
 
